15ff5449e3d3b724cdda9eecbaeb5017324a695d
BryceDixon <bryce.dixon.bd@gmail.com>
2023-11-15 18:05:50 +0000

    plotting

--- assesment2/nbody_main.py
+++ assesment2/nbody_main.py
@@ -12,32 +12,44 @@ Version: 15/11/2023
 
 import numpy as np
 from astropy.constants import G as G_grav
+import matplotlib.pyplot as plt
 
 
-def a(positions, masses):
+def a(positions, masses, use_grav = True):
     """
+    Function to calculate the gravitational accelerations acting on N number of particles at given positions
 
     Parameters
     ----------
     positions : array of floats
-        array of floats with 3 columns representing x,y,z positions and N rows representing each particle
+        2D array of floats with 3 columns representing x,y,z positions and N rows representing each particle
     masses : array of floats
-        array of particle masses in order m_1, m_2, ..., etc
+        1D array of particle masses in order m_1, m_2, ..., etc
+    use_grav : bool, optional
+        If True, the gravitational constant G will be used in acceleration calculation, If False, G will be treated as 1, essentially giving the answers in terms of G, defaults to True
     
     Returns
     -------
     a : array of floats
-        array of accellerations with 3 columns representing x,y,z accelerations and N rows representing each particle
+        2D array of accellerations with 3 columns representing x,y,z accelerations and N rows representing each particle
     """
-    G_grav = 1
     
+    if use_grav is True:
+        G = G_grav.value
+    if use_grav is False:
+        G = 1
+        
     positions = np.array(positions, dtype=float)
     masses = np.array(masses, dtype=float)
+    
+    assert len(positions[0,:]) == 3, "Number of columns in position must be equal to 3, representing x,y,z positions"
+    assert np.shape(masses) == (len(positions[:,0]),), "masses must be a 1D array or list of the particle masses and the number of particle masses given must match number of particles in position array"
+    
     a = np.zeros_like(positions)
         
-    for i in range(len(positions)):
+    for i in range(len(positions[:,0])):
         
-        for k in range(len(positions)):
+        for k in range(len(positions[:,0])):
             
             # calculate difference in x,y,z positions for particle k and i, will be 0 for when k=i and so not contribute to acceleration
             dif_pos = positions[k,:] - positions[i,:]
@@ -46,26 +58,37 @@ def a(positions, masses):
             r = ((np.linalg.norm(dif_pos))**2 + 0.00001**2)**(3/2)
             
             # calculate the x,y,z components of acceleration and add them to the acceleration of the ith particle
-            a[i,:] += ((G_grav * masses[k])/r)*dif_pos
+            a[i,:] += ((G * masses[k])/r)*dif_pos
     
     return a 
 
 
 class N_body:
     
-    def __init__(self, h, x_0, v_0, m):
+    def __init__(self, h, pos_0, v_0, m):
         
-        x_0 = np.array(x_0)
+        pos_0 = np.array(pos_0)
         v_0 = np.array(v_0)
         self.h = h
-        self.x_0 = x_0
+        self.pos_0 = pos_0
         self.v_0 = v_0
         self.m = m
     
     
-    def verlet(self):
+    def verlet(self, use_grav):
         
-        v_step = self.v_0 + 1/2 * self.h * a(self.x_0, self.m)
-        pos = self.x_0 + self.h * v_step
-        v = v_step + 1/2 * self.h * a(pos, self.m)
+        v_step = self.v_0 + 0.5 * self.h * a(self.pos_0, self.m, use_grav)
+        pos = self.pos_0 + self.h * v_step
+        v = v_step + 0.5 * self.h * a(pos, self.m, use_grav)
+    
+        self.v = v
+        self.pos = pos
+    
+    def plot(self):
+        
+        for i in range(len(self.pos[:,0])):
+            plt.plot([self.pos_0[i,0],self.pos[i,0]], [self.pos_0[i,1], self.pos[i,1]], color = 'C'+str(i))
+        
+        self.pos_0 = self.pos
+        self.v_0 = self.v
         
