6cdae6fbed6d97342b159eb3b6d06a20632e52aa
BryceDixon <bryce.dixon.bd@gmail.com>
2024-02-14 19:57:16 +0000

    keplerian checks

--- assessment3/blackhole_main.py
+++ assessment3/blackhole_main.py
@@ -11,20 +11,21 @@ import os
 
 class bh_path:
 
-    def __init__(self, init_t, init_r, init_phi, mass, init_proper_t, t_bound, max_step, first_step, atol, rtol, use_const = False):
+    def __init__(self, y, init_proper_t, t_bound, max_step, first_step, atol, rtol, use_const = False):
         """
         Class to obtain the trajectories of a particle around a black hole
 
         Parameters
         ----------
-        init_t : float
-            initial time in seconds
-        init_r : float
-            initial radial distance from the black hole in meters
-        init_phi : float
-            initial azimuthal angle around the black hole in radians
-        mass : float
-            mass of the black hole in kg
+        y : array of floats
+            1D array of the initial conditions for a keplerian orbit in the form:
+            y[0] = t
+            y[1] = dt
+            y[2] = r
+            y[3] = dr
+            y[4] = phi
+            y[5] = dphi
+            y[6] = mass
         init_proper_t : float
             initial proper time in seconds
         t_bound : float
@@ -40,8 +41,7 @@ class bh_path:
         use_const : bool, optional
             if False, sets G and c to 1 when required in calculations, if True, uses G and c values from the astropy constants library in calculations, defaults to False
         """
-    
-        y = aux.initial_y(init_t, init_r, init_phi, mass, use_const)
+
         self.y_values = [y]
         self.propt_values = [init_proper_t]
         if use_const == False:
@@ -61,14 +61,14 @@ class bh_path:
             sets how often append the results to the saving arrays, if None will save every step, this is not recommended for a large number of steps, defaults to None
         """
         
-        assert type(save_interval) == int, "save_interval must be an integer, represents the interval between steps that the data should be saved"
+        assert type(save_interval) == int or save_interval == None, "save_interval must be an integer or None, represents the interval between steps that the data should be saved"
         num = 0
         save_num = save_interval
         while self.propt_values[-1] <= self.t_bound:
             # step the integrator forward
             self.runga.step()
             if save_interval == None:
-                # append the results to teh saving arrays based on the saving interval
+                # append the results to the saving arrays based on the saving interval
                 self.y_values.append(self.runga.y)
                 self.propt_values.append(self.runga.t)
             else:
@@ -85,6 +85,9 @@ class bh_path:
                 break
             else:
                 pass
+        
+        self.y_values = np.array(self.y_values)
+        self.propt_values = np.array(self.propt_values)
             
     def plot(self, conserve_plots = True):
         """
@@ -104,9 +107,8 @@ class bh_path:
             ax = fig.add_subplot(1,1,1)
         
         # plot the main trajectories
-        y_values = np.array(self.y_values)
-        r = y_values[:,2]
-        phi = y_values[:,4]
+        r = self.y_values[:,2]
+        phi = self.y_values[:,4]
         x = r*np.cos(phi)
         y = r*np.sin(phi)
         ax.plot(x,y, color = 'b', label = 'Particle Trajectory')
@@ -125,16 +127,29 @@ class bh_path:
                 c = 1
                 G = 1
             # get constants M and R (schwartschild radius)
-            M = y_values[0,6]
+            M = self.y_values[0,6]
             R = (2*G*M)/(c**2)
             # c^2 is conserved based on the same invariant equation used to calclate the initial t velocity, this time taking into acocunt dt, dr, and dphi
-            c_conserve = (1-R/r)*y_values[:,1]**2 - (1/(1-R/r))*y_values[:,3]**2 - r**2 * y_values[:,5]**2
+            c_conserve = (1-R/r)*self.y_values[:,1]**2 - (1/(1-R/r))*self.y_values[:,3]**2 - r**2 * self.y_values[:,5]**2
             c_conserve_perc = ((c_conserve - c_conserve[0])/c_conserve[0]) * 100
             
             ax1.plot(self.propt_values, c_conserve_perc, color = 'b')
             ax1.set_xlabel('Proper Time /s')
             ax1.set_ylabel('Percentage Difference /%')
             ax1.set_title('Percentage Difference in the Conserved Quantity $c^2$ against Proper Time')
+        
+        self.x = x
+        self.y = y
+    
+    def kep_check(self):
+        
+        period_phi = self.y_values[0,4] + (np.pi * 2)
+        iter_per = (np.abs(self.y_values[:,4] - period_phi)).argmin()
+        period = self.propt_values[iter_per] - self.propt_values[0]
+        print("Period = {} Seconds".format(period))
+        
+        semi_major_axis = np.max(self.y_values[:,2])
+        print("Semi Major Axis = {} Meters".format(semi_major_axis))
     
     def save(self, savefolder):
         """
@@ -155,18 +170,18 @@ class bh_path:
 
 
 
-def trajectory(init_t, init_r, init_phi, mass, init_proper_t, t_bound, max_step, first_step, atol, rtol, use_const = False, save_interval = None, conserve_plots = True, savefolder = None):
+def trajectory(init_t, init_r, init_phi, mass, init_proper_t, t_bound, max_step, first_step, atol, rtol, y = None, use_const = False, save_interval = None, conserve_plots = True, keplerian_check = False, savefolder = None):
     """
     Function to run the bh_path class to generate and plot the trajectories of a particle around a black hole using a 4th order runge-kutta integrator method
 
     Parameters
     ----------
     init_t : float
-        initial time in seconds
+        initial time in seconds for a stable keplerian orbit
     init_r : float
-        initial radial distance from the black hole in meters
+        initial radial distance from the black hole in meters for a stable keplerian orbit
     init_phi : float
-        initial azimuthal angle around the black hole in radians
+        initial azimuthal angle around the black hole in radians for a stable keplerian orbit
     mass : float
         mass of the black hole in kg
     init_proper_t : float
@@ -181,19 +196,29 @@ def trajectory(init_t, init_r, init_phi, mass, init_proper_t, t_bound, max_step,
         absolute tolerance of the integrator, controls the absolute accuracy (number of correct decimal places)
     rtol : float
         relative tolderance of the integrator, controls the relative accuracy (number of correct digits)
+    y : array of floats, optional
+        y array of initial particle conditions, setting this will overwrite the init_t, init_r, init_phi, and mass values to be that of the array, use this to plot a custom orbit, defaults to None
     use_const : bool, optional
         if False, sets G and c to 1 when required in calculations, if True, uses G and c values from the astropy constants library in calculations, defaults to False
     save_interval : int, optional
         sets how often append the results to the saving arrays, if None will save every step, this is not recommended for a large number of steps, defaults to None
     conserve_plots : bool, optional
         if True, plots the conservation checks, if False, does not, defaults to True
+    keplerian_check : bool, optional
+        if True, prints the period and semi-major axis of the orbit, if False, does not, defaults to False
     savefolder : str
         name of the directory to save the data to
     """
     
-    bh = bh_path(init_t, init_r, init_phi, mass, init_proper_t, t_bound, max_step, first_step, atol, rtol, use_const)
+    if y == None:
+        y = aux.initial_y(init_t, init_r, init_phi, mass, use_const)
+    else:
+        assert np.shape(y) == (7,), "y must be a 1D matrix of lenght 7"
+    bh = bh_path(y, init_proper_t, t_bound, max_step, first_step, atol, rtol, use_const)
     bh.run(save_interval)
     bh.plot(conserve_plots)
+    if keplerian_check is True:
+        bh.kep_check()
     if savefolder is not None:
         bh.save(savefolder)
             
