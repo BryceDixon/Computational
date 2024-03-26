cdc523c1f8df60c94e76f621b9d835bc6341bbda
BryceDixon <bryce.dixon.bd@gmail.com>
2024-03-26 12:48:08 +0000

    run file

---
+++ assessment3/blackhole_run.py
@@ -0,0 +1,11 @@
+"""
+File to run the code for the black hole trajectories
+
+Author : Bryce Dixon
+Version : 26/03/24
+"""
+
+import blackhole_main as bh
+
+# this code is very simple to use
+bh.trajectory(input_file_dir = 'C:/Users/bryce/Python/Computational/assessment3/bh_inputs_kep.yaml')
