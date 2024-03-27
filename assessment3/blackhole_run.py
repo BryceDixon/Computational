7a1afb7a9ae02d3bd9cc7d42f65861dd6d9cb872
BryceDixon <bryce.dixon.bd@gmail.com>
2024-03-27 15:52:03 +0000

    final

--- assessment3/blackhole_run.py
+++ assessment3/blackhole_run.py
@@ -7,5 +7,7 @@ Version : 26/03/24
 
 import blackhole_main as bh
 
-# this code is very simple to use
-bh.trajectory(input_file_dir = 'C:/Users/bryce/Python/Computational/assessment3/bh_inputs_kep.yaml')
+# this code is very simple to use and runs off of the settings in the bh_inputs.yaml file
+# these should be set accordingly in the file, all of the parameter descriptions are located in that file
+# the bh.trajectory function is the only function that the user needs to call, the only parameter for this is the directory of the settings file
+bh.trajectory(input_file_dir = 'C:/Users/bryce/Python/Computational/assessment3/bh_inputs.yaml')
